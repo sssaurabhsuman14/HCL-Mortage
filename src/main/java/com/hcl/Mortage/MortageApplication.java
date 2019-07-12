0404f33ec2052e3032e4757a2b6055bc6f4db7db
saurabh suman <sssaurabhsuman@gmail.com>
2019-07-12 10:24:31 +0530

    adding sonar and swagger to the project

---
+++ src/main/java/com/hcl/Mortage/MortageApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.Mortage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortageApplication.class, args);
+	}
+
+}
